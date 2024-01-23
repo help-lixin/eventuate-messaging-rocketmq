75489aadc96d21f5da89deb0dd2af3bcf3c60ba7
lixin <13798516431@126.com>
2024-01-23 23:24:33 +0800

    完善对rocketmq的支持

--- eventuate-messaging-rocketmq-spring-common/src/main/java/help/lixin/eventuate/messaging/rocketmq/spring/common/EventuateRocketMQPropertiesConfiguration.java
+++ eventuate-messaging-rocketmq-spring-common/src/main/java/help/lixin/eventuate/messaging/rocketmq/spring/common/EventuateRocketMQPropertiesConfiguration.java
@@ -1,18 +1,22 @@
 package help.lixin.eventuate.messaging.rocketmq.spring.common;
 
 import help.lixin.eventuate.messaging.rocketmq.common.EventuateRocketMQConfigurationProperties;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.Assert;
 
 @Configuration
 public class EventuateRocketMQPropertiesConfiguration {
 
-
     @Bean
-    public EventuateRocketMQConfigurationProperties //
-    eventuateRocketMQConfigurationProperties(@Value("${eventuate.local.rocket.mq.nameserver.address:${eventuatelocal.rocket.mq.nameserver.address}}") //
-                                             String nameServerAddress) {
+    public EventuateRocketMQConfigurationProperties eventuateRocketMQConfigurationProperties(Environment environment) {
+        String nameServerAddress = null;
+        nameServerAddress = environment.getProperty("eventuatelocal.rocket.mq.nameserver.address", String.class);
+        if (null == nameServerAddress) {
+            nameServerAddress = environment.getProperty("eventuate.local.rocket.mq.nameserver.address", String.class);
+        }
+        Assert.notNull(nameServerAddress, "请配置nameserver地址.");
         return new EventuateRocketMQConfigurationProperties(nameServerAddress);
     }
 }
